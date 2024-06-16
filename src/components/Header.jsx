a957e39ececc827910dccdcd9b83c702632818b9
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-06-16 15:49:26 +0530

    components added

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <Header>
+      <h1>
+        <span className="text-slate-500">az</span>
+        <span className="text-slate-700">Estate</span>
+      </h1>
+    </Header>
+  );
+}
