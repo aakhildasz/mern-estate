545b8be9ef1d8879121487a643799f207c79deb9
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-06-16 13:52:23 +0530

    commit message of real estate website in mern

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
 
 export default function App() {
-  return <div className="text-red-500 font-bold underline text-3xl">App</div>;
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>;
 }
