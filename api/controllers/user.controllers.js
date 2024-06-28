1d008d765c3e7dafb069bbef5c06299031888adb
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-06-28 13:41:09 +0530

    add delete user functionality

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
+import { read } from "fs";
 
 export const test = (req, res) => {
   res.json({
@@ -32,6 +33,18 @@ export const updateUser = async (req, res, next) => {
     const { password, ...rest } = updateUser._doc;
 
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only delete your account"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
+    res.status(200).json("User deleted successfully");
   } catch (error) {
     next(error);
   }
