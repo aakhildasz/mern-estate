0e00c0872bb79abc35ac58742594b17f88a52766
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-07-01 12:47:49 +0530

    add create listing api route

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,7 +1,6 @@
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
-import { read } from "fs";
 
 export const test = (req, res) => {
   res.json({
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listing.route.js";
 dotenv.config();
 
 mongoose
@@ -29,6 +30,7 @@ app.listen(port, (req, res) => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // creating this for generating much more clear and formatted way to displya error
 app.use((err, req, res, next) => {
