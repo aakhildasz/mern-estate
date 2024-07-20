2c7457949469d5229aa54f1af60df994f131b6bf
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-07-20 16:26:38 +0530

    Complete show listing functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -13,6 +13,7 @@ userRouter.get("/test", test);
 userRouter.post("/update/:id", verifyToken, updateUser);
 userRouter.delete("/delete/:id", verifyToken, deleteUser);
 
-userRouter.get("/listing/:id", verifyToken, getUserListings);
+userRouter.get("/listings/:id", verifyToken, getUserListings);
+
 
 export default userRouter;
